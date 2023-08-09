c1a7a6734ec1c2dfaa8a1c03e64d6980951aceff
Pierre Noble <pierrenoble34@gmail.com>
2023-08-09 06:24:00 +0200

    lock voice room

--- src/commands/lock-voice-room.ts
+++ src/commands/lock-voice-room.ts
@@ -1,14 +1,14 @@
-import { CommandInteraction, Client, ApplicationCommandType, Collection, Snowflake, GuildMember } from "discord.js";
+import { CommandInteraction, Client, ApplicationCommandType, Collection, Snowflake, GuildMember, ComponentType, VoiceChannel, PermissionsBitField } from "discord.js";
 import { Command } from "./interfaces/command.interface";
 
 export const LockVoiceRoom: Command = {
-    name: "lock-voice-room",
+    name: "voice-lock",
     description: "Lock a voice room, other people cannot join.",
     type: ApplicationCommandType.ChatInput,
     run: async (client: Client, interaction: CommandInteraction) => {
         if (!interaction.channel.name.match(/voice room$/)) {
             interaction.followUp({
-                content: `Alors ${interaction.user.globalName}, on essaye de gruger les permissions ?\nCes salons ne peuvent pas être modifiés !\nJe t'invite à créer ton propre channel avec\n「➕」New voice\nsi tu souhaites gérer le tien !`
+                content: `Alors ${interaction.user.globalName}, on essaye de gruger les permissions ?\nCes salons ne peuvent pas être modifiés !\nJe t'invite à créer ton propre salon avec\n「➕」New voice\nsi tu souhaites gérer le tien !`
             })
 
             return;
@@ -27,17 +27,36 @@ export const LockVoiceRoom: Command = {
         const firstMember = channelMembers.at(0);
 
         if (firstMember.id === interaction.user.id) {
-            interaction.channel.edit({
-                locked: true
-            }).then(() => interaction.followUp({
+            const channel = await client.channels.fetch(interaction.channelId) as VoiceChannel;
+
+            channel.permissionOverwrites.set([
+                {
+                    id: interaction.guild.id,
+                    deny: [PermissionsBitField.Flags.SendMessages],
+                }, {
+                    id: interaction.guild.id,
+                    deny: [PermissionsBitField.Flags.ViewChannel],
+                }, {
+                    id: interaction.guild.id,
+                    deny: [PermissionsBitField.Flags.ReadMessageHistory]
+                }, {
+                    id: interaction.guild.id,
+                    deny: [PermissionsBitField.Flags.CreatePublicThreads]
+                }, {
+                    id: interaction.guild.id,
+                    deny: [PermissionsBitField.Flags.CreatePrivateThreads]
+                }
+            ]);
+
+            interaction.followUp({
                 content: 'Ce salon est désormais verrouillé'
-            }))
+            })
+
+            return;
         }
 
         interaction.followUp({
-            body: {
-                
-            }
+            content: `Seul ${firstMember.user.globalName} est autorisé à exécuter des actions dans ce salon`
         })
 
         return;
